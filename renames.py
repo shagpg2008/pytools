4cf6c431a5d00d201175567db300fe65b0bbcaf0
shagpg2008 <shagpg2008@users.noreply.github.com>
2020-01-22 15:42:52 +0800

    work currect dict and recursely

--- renames.py
+++ renames.py
@@ -15,9 +15,11 @@ def mvfiles(dir, matched_pattern, replaced_pattern):
 			new_name = _matched_file.sub(replaced_pattern, file, 1)
 			__dst = os.path.join(dir, new_name)
 			os.rename(__path, __dst);
+		elif os.path.isdir(__path):
+			mvfiles(__path, matched_pattern, replaced_pattern)
 
-if len(sys.argv) != 4:
-	print './' + os.path.basename(__file__) + '<DIR> <MATCHED_PATTERN> <REPLACED_PATTERN>'
+if len(sys.argv) != 3:
+	print( './' + os.path.basename(__file__) + '<MATCHED_PATTERN> <REPLACED_PATTERN>')
 else:
-	mvfiles(sys.argv[1], sys.argv[2], sys.argv[3])
+	mvfiles(os.getcwd(), sys.argv[1], sys.argv[2])
 	
